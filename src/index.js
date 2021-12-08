075d8c10279c85c998924cd215796699460f67ee
Kenan Kadic <ken.kadic@outlook.com>
2021-12-08 08:12:41 -0600

    SeasonDisplay.css added

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 if (module.hot) {
-    module.hot.accept();
-  }
-
-class App extends React.Component {
-    state = { lat: null, errorMessage: '' };
-
-    componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-        position => this.setState({lat: position.coords.latitude}),
-        
-        (err) => this.setState({ errorMessage: err.message }) 
-    );
-}   
- 
-render() {
-    // render just returns some JSX--that is it
-
-    if (this.state.errorMessage && !this.state.lat){
-        return <div>Error: {this.state.errorMessage}</div>
-    }
-
-    if (!this.state.errorMessage && this.state.lat){
-        return <div> <SeasonDisplay lat={this.state.lat}/></div>
-    }
-
-    return <div>Loading!</div>                    
-    }
+  module.hot.accept();
 }
 
-ReactDOM.render (
-    <App />,
-    document.querySelector('#root')
-);
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
