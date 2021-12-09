1af94d237f102e70721040522bbf5fb2e2cba03b
Kenan Kadic <ken.kadic@outlook.com>
2021-12-08 21:49:33 -0600

    border added

--- src/index.js
+++ src/index.js
@@ -17,18 +17,26 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render!!
-  render(props) {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
+        return <div>Error: {this.state.errorMessage}</div>;
+      }
+  
+      if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat} />;
+      }
+  
+      return <div><Spinner message="Waiting on location approval..."/></div>;
     }
+  
 
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <div><Spinner message="Waiting on location approval..."/></div>;
-  }
+  // React says we have to define render!!
+  render() {
+    return (
+        <div className="border red" style={{ border: '10px solid lightblue'}} >
+            {this.renderContent()}
+        </div>
+    );
 }
-
+}
 ReactDOM.render(<App />, document.querySelector("#root"));
